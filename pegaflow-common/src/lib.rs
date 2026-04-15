30fef84f8d35535674748f66115d90be86f4e7ad
susun <751080330@qq.com>
2026-04-15 16:38:27 +0800

    refactor(common): move BlockKey into pegaflow-common

--- pegaflow-common/src/lib.rs
+++ pegaflow-common/src/lib.rs
@@ -1,7 +1,11 @@
+pub mod block;
 pub mod hll;
 pub mod logging;
+#[cfg(target_os = "linux")]
 pub mod numa;
 
+pub use block::BlockKey;
+#[cfg(target_os = "linux")]
 pub use numa::{
     NumaNode, NumaTopology, format_cpu_list, pin_thread_to_numa_node, query_pages_numa,
     read_cpu_topology_from_sysfs, run_on_numa,
